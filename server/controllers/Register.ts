23780001cb3c587e6438aa46cf30142662bd2e1f
Panem <titoluccao@gmail.com>
2021-07-15 17:22:39 -0300

    added verifySyntax to Register controller

--- server/controllers/Register.ts
+++ server/controllers/Register.ts
@@ -1,12 +1,30 @@
 import type { Request, Response } from 'express'
 
 import verifyParams from '../middlewares/verifyParams'
+import verifySyntax from '../middlewares/verifySyntax'
+
+interface RequestBody {
+    email: string
+    password: string
+    register: string
+}
 
 export default async function Register(req: Request, res: Response) {
 
     try {
 
+        // Verify if the required params are present
         await verifyParams(req.body, [`email`, `password`, `register`])
+
+        const { email, password, register }: RequestBody = req.body
+
+        // Verify the parameters syntax passed
+        await verifySyntax(`email`, /^[a-z0-9.]+@[a-z0-9]+\.[a-z]+(\.[a-z]+)?$/i, email)
+        await verifySyntax(`password`, /^.{8,64}$/, password)
+        await verifySyntax(`register`, /^[0-9]{6}$/, register)
+
+        // Return to the client
+        res.send(`a`)
 
     } catch (status) {
 
