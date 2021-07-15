2885dd6a4f4d9a1f558aeac3b1ce0400cfc52aa5
Panem <titoluccao@gmail.com>
2021-07-15 16:59:52 -0300

    created Register controller

--- server/components/Routes.ts
+++ server/components/Routes.ts
@@ -1,6 +1,8 @@
 import { Router } from "express"
 import removeHeaders from '../middlewares/removeHeaders'
 
+import Register from "../controllers/Register"
+
 export default function Routes() {
 
     // Initializing Router
@@ -8,6 +10,9 @@ export default function Routes() {
 
     // Setting Up Middlewares
     routes.use(removeHeaders)
+
+    // Setting up routes
+    routes.post(`/register`, Register)
 
     return routes
 }
--- server/controllers/Register.ts
+++ server/controllers/Register.ts
@@ -2,12 +2,17 @@ import type { Request, Response } from 'express'
 
 import verifyParams from '../middlewares/verifyParams'
 
-export default async function Register(req: Request, res: Response): Promise<void> {
+export default async function Register(req: Request, res: Response) {
 
     try {
 
         await verifyParams(req.body, [`email`, `password`, `register`])
 
-    } catch (err) { }
+    } catch (status) {
+
+        if (typeof status === `string`) res.status(400).json({ status })
+        else if (typeof status === typeof Error) res.sendStatus(500)
+
+    }
 
 }
