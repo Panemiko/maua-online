316ab8c25202450421c4ee4aa812049e238faff5
Panem <titoluccao@gmail.com>
2021-07-18 21:41:25 -0300

    add: styled-components at server

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,9 +1,39 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document'
+import Document, { Html, Head, Main, NextScript, DocumentContext } from 'next/document'
+import { ServerStyleSheet } from 'styled-components'
 
 export default class AppDocument extends Document {
+
+    // Render styled-components at server
+    static async getInitialProps(ctx: DocumentContext) {
+        const sheet = new ServerStyleSheet()
+        const originalRenderPage = ctx.renderPage
+
+        try {
+            ctx.renderPage = () =>
+                originalRenderPage({
+                    enhanceApp: (App) => (props) =>
+                        sheet.collectStyles(<App {...props} />)
+                })
+
+            const initialProps = await Document.getInitialProps(ctx)
+            return {
+                ...initialProps,
+                styles: (
+                    <>
+                        {initialProps.styles}
+                        {sheet.getStyleElement()}
+                    </>
+                )
+            }
+        } finally {
+            sheet.seal()
+        }
+    }
+
+    // Page layout
     render() {
         return (
-            <Html>
+            <Html lang="pt-br">
                 <Head />
                 <link rel="preconnect" href="https://fonts.googleapis.com" />
                 <link rel="preconnect" href="https://fonts.gstatic.com" />
