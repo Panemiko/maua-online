046daf09149a30822f9fe948fee9bebd00f5458a
Panem <titoluccao@gmail.com>
2021-07-20 19:45:57 -0300

    add: NotFound page

--- src/index.tsx
+++ src/index.tsx
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 
 import RegisterPage from './pages/Register'
+import NotFound from './pages/NotFound'
 
 ReactDOM.render(
     <BrowserRouter>
         <Switch>
-            <Route path="/register" component={RegisterPage} />
+            <Route path="/register" exact={true} component={RegisterPage} />
+            <Route component={NotFound} />
         </Switch>
     </BrowserRouter>,
     document.getElementById(`root`)
---
+++ src/pages/NotFound.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const NotFound: React.FC = () => {
+
+    return (
+        <div>
+            <p>Página não encontrada</p>
+        </div>
+    )
+
+}
+
+export default NotFound
